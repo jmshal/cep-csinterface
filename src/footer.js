56335fd9b2d71afb1ddb3e3c17d45e9891a9ddcb
Jacob Marshall <git@jacobmarshall.co>
2017-10-10 09:15:13 +1300

    Initial commit

---
+++ src/footer.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+  CSXSWindowType: CSXSWindowType,
+  Version: Version,
+  VersionBound: VersionBound,
+  VersionRange: VersionRange,
+  Runtime: Runtime,
+  Extension: Extension,
+  CSEvent: CSEvent,
+  SystemPath: SystemPath,
+  ColorType: ColorType,
+  RGBColor: RGBColor,
+  Direction: Direction,
+  GradientStop: GradientStop,
+  GradientColor: GradientColor,
+  UIColor: UIColor,
+  AppSkinInfo: AppSkinInfo,
+  HostEnvironment: HostEnvironment,
+  HostCapabilities: HostCapabilities,
+  ApiVersion: ApiVersion,
+  CSInterface: CSInterface,
+};
